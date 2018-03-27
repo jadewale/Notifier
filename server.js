0dfc8812c103a794f91afdec9d0a72a9f4636c5c
Jolaade Adewale <jolaade.adewale@andela.com>
2018-03-27 22:44:02 +0100

    add default route

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
 app.listen(port);
+app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to The Job!' }));
 
 setInterval(services.intervalFunc, 1500);
 setInterval(services.clearDatabase, (60000 * 360));
 services.clearDatabase();
+
 
 module.exports = app;
